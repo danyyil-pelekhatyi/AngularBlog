58a0e45fee60001d8382e408ee662b9907350c90
Andrii <andriy.vandakurov@gmail.com>
2016-06-23 17:23:02 +0300

    Update tests

--- app/tests/MainControllerTest.js
+++ app/tests/MainControllerTest.js
@@ -1,20 +1,18 @@
-"use strict";
-require('../controllers/MainController')
-describe("App", function () {
-	beforeEach(angular.module('app'));
+describe("MainController", function () {
+	var $controller,
+		$scope;
 
-	describe("MainController", function () {
-		var $controller,
-			$scope;
+	beforeEach(function(){
+		angular.mock.module('app');
 
-		beforeEach(inject(function(_$controller_, $rootScope){
-			var $scope = $rootScope.$new();;
+		inject(function(_$controller_, $rootScope){
+			$scope = $rootScope.$new();
 			$controller = _$controller_('MainController', { $scope: $scope });
-		}));
+		})
+	});
 
-		it("has the correct message", function () {
-			var message = 'Two birds killed with one stone!';
-			expect($scope.message).toBe(message);
-		});
+	it("has the correct message", function () {
+		var message = 'Two birds killed with one stone!';
+		expect($scope.message).toBe(message);
 	});
 });
